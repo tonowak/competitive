32b7e12457adaa4c68bb40fe986f6499f647fa56
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2018-12-01 20:30:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cd ~
-git clone https://github.com/tonowak/competitive/tree/master/src
+git clone https://github.com/tonowak/competitive/
 cp competitive/src ~ -r
 cp competitive/src/utils/vimrc ~/.vimrc
 mv competitive ~/src/.github
