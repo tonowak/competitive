34f470cc0ac02ccd37fcd644bb646fed02f920fd
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2018-12-02 14:20:30 +0100

    Adding note about OItime in install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ cp competitive/src ~ -r
 cp competitive/src/utils/vimrc ~/.vimrc
 mv competitive ~/src/.github
 echo "source ~/src/aliases.sh" >> ~/.zshrc
+echo "Jeżeli tego jeszcze nie zrobiłeś/zrobiłaś, wpisz w konsoli: sudo sysctl kernel.perf_event_paranoid=-1"
