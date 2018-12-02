b96a75886d2d654cc51eb7906d10f35ea52f23e1
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2018-12-02 22:03:21 +0100

    Added better vim colors

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 cd ~
 git clone https://github.com/tonowak/competitive/
 cp competitive/src ~ -r
+# vim
 cp competitive/src/utils/vimrc ~/.vimrc
+mkdir -p "~/.vim/colors/"
+cp competitive/src/utils/one.vim ~/.vim/colors/
 mv competitive ~/src/.github
 echo "source ~/src/aliases.sh" >> ~/.zshrc
 echo "Jeżeli tego jeszcze nie zrobiłeś/zrobiłaś, wpisz w konsoli: sudo sysctl kernel.perf_event_paranoid=-1"
