0426ada4d3185c82d10c7d31dbfe169032b49b3d
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2018-12-03 15:05:06 +0100

    Changing ~ to $HOME

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
-cd ~
+cd "$HOME/"
 git clone https://github.com/tonowak/competitive/
-cp competitive/src ~ -r
+cp competitive/src "$HOME/" -r
+
 # vim
-cp competitive/src/utils/vimrc ~/.vimrc
-mkdir -p "~/.vim/colors/"
-cp competitive/src/utils/one.vim ~/.vim/colors/
-mv competitive ~/src/.github
-echo "source ~/src/aliases.sh" >> ~/.zshrc
+cp competitive/src/utils/vimrc "$HOME/.vimrc"
+mkdir -p "$HOME/.vim/colors/"
+cp competitive/src/utils/one.vim "$HOME/.vim/colors/"
+
+mv competitive "$HOME/src/.github"
+echo "source $HOME/src/aliases.sh" >> ~/.zshrc
 echo "Jeżeli tego jeszcze nie zrobiłeś/zrobiłaś, wpisz w konsoli: sudo sysctl kernel.perf_event_paranoid=-1"
